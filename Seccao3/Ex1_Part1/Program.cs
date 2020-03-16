3af9094b448cfa5644fdb71dbdd6f3b1aebf4488
Isabel Sousa <belinhasousa.1@gmail.com>
2020-03-16 15:53:16 +0000

    Exercise 1, Part1, Section 3

---
+++ Seccao3/Ex1_Part1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Ex1_Part1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int value1, value2;
+            int valuesSum = 0;
+
+            value1 = int.Parse(Console.ReadLine());
+            value2 = int.Parse(Console.ReadLine());
+
+            valuesSum = value1 + value2;
+
+            Console.WriteLine($"SUM = {valuesSum}");
+
+        }
+    }
+}
