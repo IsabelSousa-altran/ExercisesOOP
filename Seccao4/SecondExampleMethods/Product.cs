5c3abe0955bf9824de9f76c24422515528fbb43b
Isabel Sousa <belinhasousa.1@gmail.com>
2020-03-17 15:53:56 +0000

    Second Exercise(example) with methods (Product exercise)

---
+++ Seccao4/SecondExampleMethods/Product.cs
@@ -0,0 +1,27 @@
+﻿using System.Globalization;
+
+namespace SecondExampleMethods
+{
+    class Product
+    {
+        public string Name;
+        public double Price;
+        public int StockQuantity;
+
+        public double TotalValueInStock()
+        {
+            return Price * StockQuantity;
+        }
+
+        public override string ToString()
+        {
+            return Name 
+                + ", $ " 
+                + Price.ToString("F2", CultureInfo.InvariantCulture)
+                + ", "
+                + StockQuantity
+                + " unitys, Total: $ "
+                + TotalValueInStock().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ Seccao4/SecondExampleMethods/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace SecondExampleMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product = new Product();
+
+            Console.WriteLine("First employee data: ");
+            Console.Write("Name: ");
+            product.Name = Console.ReadLine();
+            Console.Write("Price: ");
+            product.Price = double.Parse(Console.ReadLine());
+            Console.Write("Name: ");
+            product.StockQuantity = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Product Data: {product}");
+        }
+    }
+}
