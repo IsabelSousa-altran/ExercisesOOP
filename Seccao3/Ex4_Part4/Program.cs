985de40fb2b8fc58b2ed79a3de2ad52890941964
Isabel Sousa <belinhasousa.1@gmail.com>
2020-03-17 11:45:25 +0000

    Ex4, Part4, Seccão3, for - Division

---
+++ Seccao3/Ex4_Part4/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Globalization;
+
+namespace Ex4_Part4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int quantityNumbers;
+            quantityNumbers = int.Parse(Console.ReadLine());
+            double divison = 0;
+
+            for (int i = 0; i < quantityNumbers; i++)
+            {
+                string[] numbers = Console.ReadLine().Split(' ');
+
+                double value1 = double.Parse(numbers[0]);
+                double value2 = double.Parse(numbers[1]);
+
+                if (value2 != 0)
+                {
+                    divison = value1 / value2;
+                    Console.WriteLine(divison.ToString("F1", CultureInfo.InvariantCulture));
+                }
+                else
+                {
+                    Console.WriteLine("Division Impossible");
+                }
+                
+            }
+        }
+    }
+}
