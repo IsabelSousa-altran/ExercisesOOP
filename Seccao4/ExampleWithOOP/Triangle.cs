7193b2f90b18665dde8758ed97700cb9e0d69369
Isabel Sousa <belinhasousa.1@gmail.com>
2020-03-17 14:48:39 +0000

    Code example using OOP

---
+++ Seccao4/ExampleWithOOP/Triangle.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExampleWithOOP
+{
+    class Triangle
+    {
+        //Public - Estes atributos poderão ser acessados noutro arquivo (neste caso pelo programa principal)
+        public double sideA;
+        public double sideB;
+        public double sideC;
+
+    }
+}
