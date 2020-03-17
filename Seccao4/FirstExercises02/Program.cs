1a72b36d7c60b9887e8ebc1b7ecde1c36aa60ac6
Isabel Sousa <belinhasousa.1@gmail.com>
2020-03-17 15:16:24 +0000

    First Exercises - 02 employees, classes, objects and attributes

---
+++ Seccao4/FirstExercises02/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+
+namespace FirstExercises02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double salaryMean = 0;
+
+            Employee firstEmployee = new Employee();
+
+            Console.WriteLine("First employee data: ");
+            Console.Write("Name: ");
+            firstEmployee.Name = Console.ReadLine();
+            Console.Write("Salary: ");
+            firstEmployee.Salary = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Employee secondEmployee = new Employee();
+
+            Console.WriteLine("Second employee data: ");
+            Console.Write("Name: ");
+            secondEmployee.Name = Console.ReadLine();
+            Console.Write("Salary: ");
+            secondEmployee.Salary = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            salaryMean = (firstEmployee.Salary + secondEmployee.Salary) / 2.0;
+
+            Console.WriteLine($"Average Salary: {salaryMean.ToString("F2", CultureInfo.InvariantCulture)}");
+        }
+    }
+}
