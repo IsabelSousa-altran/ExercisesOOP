1e072e581e8beaf4fb47ceec5a98960309a6c30b
Isabel Sousa <belinhasousa.1@gmail.com>
2020-03-17 17:00:56 +0000

    Proposed Exercises 03 - Student Approved or not approved

---
+++ Seccao4/ProposedExercise03/Student.cs
@@ -0,0 +1,41 @@
+﻿using System.Globalization;
+
+namespace ProposedExercise03
+{
+    class Student
+    {
+        public string Name;
+        public double FirstTrimester;
+        public double SecondTrimester;
+        public double ThirdTrimester;
+
+        public double FinalGrade()
+        {
+            return FirstTrimester + SecondTrimester + ThirdTrimester;
+        }
+
+        public bool ApprovedOrNotApproved()
+        {
+            if (FinalGrade() >= 60.0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public double MissingPoints()
+        {
+            if (ApprovedOrNotApproved())
+            {
+                return 0.0;
+            }
+            else
+            {
+                return 60.0 - FinalGrade();
+            }
+        }
+    }
+}
