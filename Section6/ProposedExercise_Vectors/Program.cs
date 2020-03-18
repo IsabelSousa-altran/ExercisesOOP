52002d56f4bc2cd894fb30f2d5fcb52d3881d885
Isabel Sousa <belinhasousa.1@gmail.com>
2020-03-18 17:35:15 +0000

    Proposed Exercise vectors

---
+++ Section6/ProposedExercise_Vectors/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace ProposedExercise_Vectors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("How many rooms will be rented? ");
+            int studentsNumber = int.Parse(Console.ReadLine());
+
+            Student[] students = new Student[10];
+
+            for (int i = 1; i <= studentsNumber; i++)
+            {
+                Console.WriteLine($"Rent #{i}: ");
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+                Console.Write("Email: ");
+                string email = Console.ReadLine();
+                Console.Write("Room: ");
+                int roomNumber = int.Parse(Console.ReadLine());
+
+                Console.WriteLine();
+
+                students[roomNumber] = new Student(name, email);
+                
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Busy rooms: ");
+            for (int i = 0; i < 10; i++)
+            {
+                if (students[i] != null)
+                {
+                    Console.WriteLine($"{i}: {students[i]}");
+                }
+                
+            }
+            
+           
+        }
+    }
+}
