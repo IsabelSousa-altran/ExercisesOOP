e7a76183f2f139f1b0418abc57b3000ebdd8cb60
Isabel Sousa <belinhasousa.1@gmail.com>
2020-03-18 12:41:28 +0000

    Encapsulation - Auto Properties

---
+++ Seccao5/Encapsulation_AutoProperties/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Encapsulation_AutoProperties
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product = new Product("Tv", 500.00, 10);
+
+            product.Name = "T";
+
+            Console.WriteLine(product.Name);
+            Console.WriteLine(product.Price);
+        }
+    }
+}
