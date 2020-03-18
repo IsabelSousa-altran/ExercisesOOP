d9ee4c4cf778a381eb0d5e403402e4d4eab2aded
Isabel Sousa <belinhasousa.1@gmail.com>
2020-03-18 19:00:42 +0000

    Parameter Modifier - Ref e out, o exercicio de ref está comentado

---
+++ Section6/Ref_Out/Calculator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ref_Out
+{
+    class Calculator
+    {
+        //REF
+        /* public static void Triple(ref int x)
+           {
+               x = x * 3;
+           }*/
+
+        //OUT
+        public static void Triple(int origin, out int result)
+        {
+            result = origin * 3;
+        }
+    }
+}
---
+++ Section6/Ref_Out/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Ref_Out
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // ref - faz o parâmtro ser uma referência para a variável original
+         /* int a = 10;
+            Calculator.Triple(ref a);
+            Console.WriteLine(a); */
+
+            // Out - Semelhante ao REF mas não exige que a variável seja iniciada
+            int a = 10;
+            int triple;
+            Calculator.Triple(a, out triple);
+            Console.WriteLine(triple);
+
+
+        }
+    }
+}
