eebfb98a1bc0fd3b1edc3c72621396cef5f7d03e
Isabel Sousa <belinhasousa.1@gmail.com>
2020-03-18 16:15:45 +0000

    Nullable)

---
+++ Section6/Nullable/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Nullable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // ? - nullable
+            double? x = null;
+            double? y = 10;
+
+            // Se alguma das variáveis for nula altera esse valor para o estipulado
+            double a = x ?? 5;
+            double b = y ?? 5;
+
+            
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+        }
+    }
+}
