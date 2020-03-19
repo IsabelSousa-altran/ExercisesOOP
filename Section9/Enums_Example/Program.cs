7747c11595604d2e1b13cd0909dd1787943a70b7
Isabel Sousa <belinhasousa.1@gmail.com>
2020-03-19 18:22:56 +0000

    Enums_Example

---
+++ Section9/Enums_Example/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Enums_Example.Entities;
+using Enums_Example.Entities.Enums;
+
+namespace Enums_Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Order order = new Order
+            {
+                Id = 1080,
+                Moment = DateTime.Now,
+                Status = OrderStatus.PendingPayment
+            };
+
+            Console.WriteLine(order);
+
+            string txt = OrderStatus.PendingPayment.ToString();
+
+            OrderStatus order1 = Enum.Parse<OrderStatus>("Delivered");
+
+            Console.WriteLine(order1);
+            Console.WriteLine(txt);
+
+        }
+    }
+}
