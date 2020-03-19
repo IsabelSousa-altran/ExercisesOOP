fb59c1a02acbba4e51df74bae407b384b6d19051
Isabel Sousa <belinhasousa.1@gmail.com>
2020-03-19 12:29:09 +0000

    Lists - ProposedExercise

---
+++ Section6/ProposedExercise_Lists/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace ProposedExercise_Lists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("How many employees will be registered? ");
+            int numberEmployees = int.Parse(Console.ReadLine());
+
+            List<Employee> employees = new List<Employee>();
+
+            for (int i = 1; i <= numberEmployees; i++)
+            {
+                Console.WriteLine($"Employee #{i}:");
+
+                Console.Write("Id: ");
+                int id = int.Parse(Console.ReadLine());
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+                Console.Write("Salary: ");
+                double salary = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                employees.Add(new Employee(id, name, salary));
+                Console.WriteLine();
+            }
+
+            Console.Write("Enter the employee id that will have salary increase: ");
+            int idIncreaseSalary = int.Parse(Console.ReadLine());
+
+            Employee employee = employees.Find(X => X.Id == idIncreaseSalary);
+
+            if (employee != null)
+            {
+                Console.Write("Enter the percentage: ");
+                double percentage = double.Parse(Console.ReadLine(), CultureInfo.CurrentCulture);
+                employee.IncreasedSalary(percentage);
+            }
+            else
+            {
+                Console.WriteLine("This id does not exist!");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Updated list of employees: ");
+            foreach (Employee employee1 in employees)
+            {
+                Console.WriteLine(employee1);
+            }
+        }
+    }
+}
