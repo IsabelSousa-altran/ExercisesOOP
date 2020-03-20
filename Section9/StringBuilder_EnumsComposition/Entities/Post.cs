6caacd005f5e1be66607308b71861ffba8ff441d
Isabel Sousa <belinhasousa.1@gmail.com>
2020-03-20 12:54:07 +0000

    String Builder, composition - Example

---
+++ Section9/StringBuilder_EnumsComposition/Entities/Post.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StringBuilder_EnumsComposition.Entities
+{
+    class Post
+    {
+        public DateTime Moment { get; set; }
+        public String Title { get; set; }
+        public string Content { get; set; }
+        public int Likes { get; set; }
+        public List<Comment> ListComments { get; set; } = new List<Comment>();
+
+        public Post(DateTime moment, string title, string content, int likes)
+        {
+            Moment = moment;
+            Title = title;
+            Content = content;
+            Likes = likes;
+            ListComments = new List<Comment>();
+        }
+
+        public void AddComment(Comment comment)
+        {
+            ListComments.Add(comment);
+        }
+
+        public void RemoveComment(Comment comment)
+        {
+            ListComments.Remove(comment);
+        }
+
+        public override string ToString()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.AppendLine(Title);
+            stringBuilder.Append(Likes);
+            stringBuilder.Append(" likes - ");
+            stringBuilder.AppendLine(Moment.ToString("dd/MM/yyyy HH:mm:ss"));
+            stringBuilder.AppendLine(Content);
+            stringBuilder.AppendLine("Comments:");
+            foreach (Comment comment in ListComments)
+            {
+                stringBuilder.AppendLine(comment.Text);
+            }
+            return stringBuilder.ToString();
+        }
+    }
+}
---
+++ Section9/StringBuilder_EnumsComposition/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using StringBuilder_EnumsComposition.Entities;
+
+namespace StringBuilder_EnumsComposition
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Comment comment1 = new Comment("Have a nice trip!");
+            Comment comment2 = new Comment("Wow that's awsome!");
+
+            Post post1 = new Post
+                (
+                    DateTime.Parse("06/21/2018 13:05:44"),
+                    "Traveling to New Zeland", 
+                    "I'm going to visit this wonderful country!",
+                    12
+                );
+
+            post1.AddComment(comment1);
+            post1.AddComment(comment2);
+
+            Comment comment3 = new Comment("Good night");
+            Comment comment4 = new Comment("May the force be with you");
+
+            Post post2 = new Post
+                (
+                    DateTime.Parse("07/28/2018 13:14:19"),
+                    "Good night guys", 
+                    "See you tomorrow",
+                    5
+                );
+
+            post2.AddComment(comment3);
+            post2.AddComment(comment4);
+
+            Console.WriteLine(post1);
+            Console.WriteLine(post2);
+
+        }
+    }
+}
