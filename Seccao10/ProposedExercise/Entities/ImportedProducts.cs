528a43c47e1b9c5e9f7eed9a624c3bd2b051cd9a
Isabel Sousa <belinhasousa.1@gmail.com>
2020-03-23 14:19:34 +0000

    Proposed Exercise - Inheritance and polimorfism

---
+++ Seccao10/ProposedExercise/Entities/ImportedProducts.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Globalization;
+using System.Text;
+
+namespace ProposedExercise.Entities
+{
+    class ImportedProducts : Product
+    {
+        public double CustomsFee { get; set; }
+
+        public ImportedProducts()
+        {
+        }
+
+        public ImportedProducts(string name, double price, double customsFee)
+            :base(name, price)
+        {
+            CustomsFee = customsFee;
+        }
+
+        public double TotalPrice()
+        {
+            return Price + CustomsFee;
+        }
+
+        public override string PriceTag()
+        {
+            return Name
+                + " $ "
+                +TotalPrice().ToString("F2", CultureInfo.InvariantCulture)
+                + " (Customs fee: $ "
+                + CustomsFee.ToString("F2", CultureInfo.InvariantCulture)
+                + ")";
+        }
+    }
+}
