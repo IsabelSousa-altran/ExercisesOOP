e2921db7d080d0c32b3f41f3e9fa343872049518
Isabel Sousa <belinhasousa.1@gmail.com>
2020-03-23 15:38:05 +0000

    Abstract Methods and Classes - Examples

---
+++ Seccao10/AbstractClasses_Example/Entities/Account.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AbstractClasses_Example.Entities
+{
+    abstract class Account
+    {
+        public int Number { get; private set; }
+        public string Holder { get; set; }
+        public double Balance { get; protected set; }
+
+        public Account()
+        {
+        }
+
+        public Account(int number, string holder, double balance)
+        {
+            Number = number;
+            Holder = holder;
+            Balance = balance;
+        }
+
+        // Virtual - vai permitir que nas outras classes seja possivel criar um override deste método
+        public virtual void Withdraw(double amount)
+        {
+            Balance -= amount + 5.0;
+        }
+
+        public void Deposit(double amount)
+        {
+            Balance += amount;
+        }
+    }
+}
---
+++ Seccao10/AbstractMethods_ResolvedExercise/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using AbstractMethods_ResolvedExercise.Entities;
+using AbstractMethods_ResolvedExercise.Entities.Enums;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace AbstractMethods_ResolvedExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Shape> listShapes = new List<Shape>();
+
+            Console.Write("Enter the number of shapes: ");
+            int numberShapes = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= numberShapes; i++)
+            {
+                Console.WriteLine($"Shape #{i} data: ");
+
+                Console.Write("Rectangle or circle (r/c)? ");
+                char shape = Char.Parse(Console.ReadLine());
+
+                Console.Write("Color (Black/Blue/Red): ");
+                Color color = Enum.Parse<Color>(Console.ReadLine());
+
+                if (shape == 'r' || shape == 'R')
+                {
+                    Console.Write("Width: ");
+                    double width = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                    Console.Write("Height: ");
+                    double height = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                    Shape rectangle = new Rectangle(color, width, height);
+                    listShapes.Add(rectangle);
+                }
+                else 
+                {
+                    Console.Write("Radius: ");
+                    double radius = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                    Shape Circle = new Circle(color, radius);
+                    listShapes.Add(Circle);
+                }
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("SHAPE AREAS: ");
+            foreach (Shape item in listShapes)
+            {
+                Console.WriteLine(item.Area().ToString("F2", CultureInfo.InvariantCulture));
+            }
+            
+        }
+    }
+}
