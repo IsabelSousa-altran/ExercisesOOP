7f3e755867fe8ada271a7b2787e93ecb636892b0
Isabel Sousa <belinhasousa.1@gmail.com>
2020-03-23 11:45:55 +0000

    Classes e métodos selados

---
+++ Seccao10/Class_MethodsSealed/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Class_MethodsSealed
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
