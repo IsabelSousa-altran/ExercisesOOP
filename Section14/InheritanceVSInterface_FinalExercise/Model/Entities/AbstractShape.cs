8b8f8c0588ba26589b2e31208e78e95ce21fc944
Isabel Sousa <belinhasousa.1@gmail.com>
2020-03-28 17:12:06 +0000

    Final exercise - Inheritance and Interface

---
+++ Section14/InheritanceVSInterface_FinalExercise/Model/Entities/AbstractShape.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using InheritanceVSInterface_FinalExercise.Model.Enum;
+
+namespace InheritanceVSInterface_FinalExercise.Model.Entities
+{
+    abstract class AbstractShape : IShape
+    {
+        public Color Color { get; set; }
+
+        public abstract double Area();
+    }
+}
---
+++ Section14/InheritanceVSInterface_FinalExercise/Model/Entities/IShape.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InheritanceVSInterface_FinalExercise.Model.Entities
+{
+    interface IShape
+    {
+        double Area();
+    }
+}
