2d784f19f3eb0c7232f9308508cc288b08f9fd1f
Isabel Sousa <belinhasousa.1@gmail.com>
2020-03-28 18:53:02 +0000

    Special Topics - String Extensions and DateTime Extensions

---
+++ Section15/SpecialTopica2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using SpecialTopica2.Extensions;
+
+namespace SpecialTopica2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String s1 = "Good morning dear students!";
+            Console.WriteLine(s1.Cut(10));
+        }
+    }
+}
---
+++ Section15/SpecialTopics/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using SpecialTopics.Extensions;
+
+namespace SpecialTopics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dateTime = new DateTime(2020, 03, 28, 8, 10, 45);
+            Console.WriteLine(dateTime.ElapsedTime());
+        }
+    }
+}
