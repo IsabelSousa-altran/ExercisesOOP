a33f2930bcb294d617458f40f17e04350f7cae55
Isabel Sousa <belinhasousa.1@gmail.com>
2020-03-28 18:12:37 +0000

    IComparable

---
+++ Section14/Icomparable_Example/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using Icomparable_Example.Entities;
+
+namespace Icomparable_Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Users\isantossousa\Documents\Altran\FF\Academia\OOPCourseTeorics\Section14\in.txt";
+
+            try
+            {
+                using (StreamReader streamReader = File.OpenText(path)) 
+                {
+                    List<Employee> list = new List<Employee>();
+                    while (!streamReader.EndOfStream)
+                    {
+                        list.Add(new Employee(streamReader.ReadLine()));
+                    }
+                    list.Sort();
+                    foreach (Employee item in list)
+                    {
+                        Console.WriteLine(item);
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occurred");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
