c92bcaece2fa491b69baf9a403ea6719db2a713f
Isabel Sousa <belinhasousa.1@gmail.com>
2020-03-29 18:09:38 +0100

    Delegate - Func

---
+++ ComparisonExample/Delegat_Func/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Delegat_Func.Entities;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Delegat_Func
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> list = new List<Product>();
+
+            list.Add(new Product("Tv", 900.00));
+            list.Add(new Product("Mouse", 50.00));
+            list.Add(new Product("Tablet", 350.50));
+            list.Add(new Product("HD Case", 80.90));
+
+            List<string> result = list.Select(NameUpper).ToList();
+            foreach (string item in result)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        static string NameUpper(Product product)
+        {
+            return product.Name.ToUpper();
+        }
+    }
+}
