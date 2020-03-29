4088aefe4cdb94ffb7da6d888403bc8f0c3e9d63
Isabel Sousa <belinhasousa.1@gmail.com>
2020-03-29 17:18:57 +0100

    Comparison<T>

---
+++ ComparisonExample/ComparisonExample/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using ComparisonExample.Entities;
+using System.Collections.Generic;
+
+namespace ComparisonExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> listProducts = new List<Product>();
+
+            listProducts.Add(new Product("TV", 900.00));
+            listProducts.Add(new Product("Notebook", 1200.00));
+            listProducts.Add(new Product("Tablet", 450.00));
+
+            listProducts.Sort(CompareProducts);
+
+            foreach (Product item in listProducts)
+            {
+                Console.WriteLine(item);
+            }
+
+            //Delegate
+            static int CompareProducts(Product product1, Product product2)
+            {
+                return product1.Name.ToUpper().CompareTo(product2.Name.ToUpper());
+            }
+        }
+    }
+}
