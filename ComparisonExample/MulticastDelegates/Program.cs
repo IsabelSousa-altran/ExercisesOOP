5259313ce0c67028be0e608339d2ef4e8a283960
Isabel Sousa <belinhasousa.1@gmail.com>
2020-03-29 17:43:37 +0100

    Multicast Delegate

---
+++ ComparisonExample/MulticastDelegates/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using MulticastDelegates.Services;
+
+namespace MulticastDelegates
+{
+    delegate void BinaryNumericOperations(double n1, double n2);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = 10;
+            double b = 12;
+
+            BinaryNumericOperations op = CalculationService.ShowSum;
+            op += CalculationService.ShowMax;
+
+            op.Invoke(a, b);
+
+        }
+    }
+}
