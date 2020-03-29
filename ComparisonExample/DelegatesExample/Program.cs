d02de078f8793e654e9f3b7765f3af8f73972fa9
Isabel Sousa <belinhasousa.1@gmail.com>
2020-03-29 17:35:54 +0100

    Delegates - Introduction

---
+++ ComparisonExample/DelegatesExample/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using DelegatesExample.Services;
+
+namespace DelegatesExample
+{
+    delegate double BinaryNumericOperations(double n1, double n2);
+    // não é possivel colocar o método square pois não coincide com o delegate criado anteriormente
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = 10;
+            double b = 12;
+
+            BinaryNumericOperations op = CalculationService.Sum;
+
+           // double result = op(a, b);
+            double result = op.Invoke(a, b);
+            Console.WriteLine(result);
+        }
+    }
+}
